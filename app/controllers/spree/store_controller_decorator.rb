5c35d9c0bd957f650d89ea0a23eeab7bde80d51e
Tibo Mogul <tibo.mogul@gmail.com>
2018-02-01 06:33:51 +0800

    Remove the error for fresh_when  Couldn't find template for digesting: spree/store/cart_link

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,6 +1,14 @@
 Spree::StoreController.class_eval do
   before_action :check_sponsor
 
+  # 2018-02-01 We are getting errors
+  # 'Couldn't find template for digesting: spree/store/cart_link'
+  # just needed to add `template: false` to fresh_when call
+  def cart_link
+    render partial: 'spree/shared/link_to_cart'
+    fresh_when(simple_current_order, template: false)
+  end
+
   private
   def check_sponsor
     session[:sponsor] ||= params[:sponsor]
