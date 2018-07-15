028a3ea99a863a2837ab8cdf408db1dcbdb6aa23
Tibo Mogul <tibo.mogul@gmail.com>
2018-07-15 14:02:44 +1000

    move content back to vida

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Spree::Core::Engine.add_routes do
-  get 'coming-soon', to: 'home#coming_soon', as: 'coming_soon'
-  get 'vida-woman', to: 'home#vida_woman', as: 'vida_woman'
   get ':sponsor/recommends/:id', to: 'products#show', as: 'recommends'
   # Add your extension routes here
   namespace :admin, path: Spree.admin_path do
