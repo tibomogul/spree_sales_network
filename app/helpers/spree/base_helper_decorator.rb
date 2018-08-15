bdce6da569597819692ea769007dddf719e79291
Tibo Mogul <tibo.mogul@gmail.com>
2018-08-16 06:22:31 +1000

    add options to product share url

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,12 +1,12 @@
 module Spree
   BaseHelper.module_eval do
-    def product_share_url(product)
+    def product_share_url(product, options={})
       if spree_current_user && !spree_current_user.sales_network_slug.blank?
-        url = spree.recommends_url(product, sponsor: spree_current_user.sales_network_slug)
+        url = spree.recommends_url(product, options.merge(sponsor: spree_current_user.sales_network_slug))
       elsif current_sponsor.present?
-        url = spree.recommends_url(product, sponsor: current_sponsor)
+        url = spree.recommends_url(product, options.merge(sponsor: current_sponsor))
       else
-        url = spree.product_url(product)
+        url = spree.product_url(product, options)
       end
     end
 
