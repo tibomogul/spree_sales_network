bf0aa10d3ca350df0cbb5969c2d8480710375ed5
Tibo Mogul <tibo.mogul@gmail.com>
2018-08-27 21:35:00 +1000

    update the tests

--- spec/models/spree/spree_commission_spec.rb
+++ spec/models/spree/spree_commission_spec.rb
@@ -15,50 +15,51 @@ describe Spree::Commission do
         payouts = Spree::Commission.compute_from_order order
         price = order.line_items.to_a.sum(&:total)
         expect(payouts.count).to eq(3)
-        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: (price*0.10).to_d.round(2)})
-        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: (price*0.05).to_d.round(2)})
-        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: (price*0.01).to_d.round(2)})
+        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: (price*0.10).to_d.round(2), rate: 10.0, base_price: price})
+        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: (price*0.05).to_d.round(2), rate: 5.0, base_price: price})
+        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: (price*0.01).to_d.round(2), rate: 1.0, base_price: price})
       end
     end
 
     describe "one line_item 5 qty for 50" do
-      let(:order) { create(:order_with_line_item_quantity, user: user,
-        line_items_price: BigDecimal.new(50), line_items_quantity: 5) }
+      let(:order) { create(:order_with_line_items, user: user,
+        line_items_price: BigDecimal.new(50), line_items_count: 5) }
 
       it "expects to compute comissions from the order" do
+        expect(order.item_total).to eq(250)
         payouts = Spree::Commission.compute_from_order order
         expect(payouts.count).to eq(3)
-        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: 25 })
-        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: 12.5 })
-        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: 2.5 })
+        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: 25, rate: 10.0, base_price:  BigDecimal.new(250)})
+        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: 12.5, rate: 5.0, base_price:  BigDecimal.new(250)})
+        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: 2.5, rate: 1.0, base_price:  BigDecimal.new(250)})
       end
     end
 
     describe "one line_item 1 qty for 19.99" do
-      let(:order) { create(:order_with_line_item_quantity, user: user,
-        line_items_price: '19.99', line_items_quantity: 1) }
+      let(:order) { create(:order_with_line_items, user: user,
+        line_items_price: BigDecimal.new('19.99'), line_items_count: 1) }
 
       it "expects to compute comissions from the order" do
         payouts = Spree::Commission.compute_from_order order
         expect(payouts.count).to eq(3)
         expect(order.line_items.sum(&:total)).to eq(19.99)
-        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: 2 })
-        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: 1 })
-        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: 0.20 })
+        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: 2, rate: 10.0, base_price:  BigDecimal.new("19.99") })
+        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: 1, rate: 5.0, base_price:  BigDecimal.new("19.99") })
+        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: 0.20, rate: 1.0, base_price:  BigDecimal.new("19.99") })
       end
     end
 
     describe "one line_item 1 qty for 19.50" do
-      let(:order) { create(:order_with_line_item_quantity, user: user,
-        line_items_price: '19.50', line_items_quantity: 1) }
+      let(:order) { create(:order_with_line_items, user: user,
+        line_items_price: '19.50', line_items_count: 1) }
 
       it "expects to compute comissions from the order" do
         payouts = Spree::Commission.compute_from_order order
         expect(payouts.count).to eq(3)
         expect(order.line_items.sum(&:total)).to eq(19.50)
-        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: 1.95 })
-        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: 0.98 })
-        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: 0.20 })
+        expect(payouts[0]).to eq({user: sponsor1, order: order, amount: 1.95, rate: 10.0, base_price:  BigDecimal.new("19.50") })
+        expect(payouts[1]).to eq({user: sponsor2, order: order, amount: 0.98, rate: 5.0, base_price:  BigDecimal.new("19.50") })
+        expect(payouts[2]).to eq({user: sponsor3, order: order, amount: 0.20, rate: 1.0, base_price:  BigDecimal.new("19.50") })
       end
     end
   end
