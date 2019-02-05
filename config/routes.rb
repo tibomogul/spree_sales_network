6eb4dd646a4ad8e803b4bb5fb7d112a2160b69f8
kennedymogul <jackieokennedy315@gmail.com>
2019-02-05 16:19:51 +1000

    Added paypal payment report in admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@ Spree::Core::Engine.add_routes do
   # Add your extension routes here
   namespace :admin, path: Spree.admin_path do
     resources :commissions
+    
+    resources :reports, only: [:index] do
+      collection do
+        get :paypal_payment_report
+        post :paypal_payment_report
+      end
+    end
   end
+
+  
+
 end
