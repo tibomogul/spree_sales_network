b2b646edee3391c30626d9eff3f44be34a072f53
kennedymogul <jackieokennedy315@gmail.com>
2019-10-03 12:23:36 +1000

    Added encashments to admin

--- app/models/spree/encashment.rb
+++ app/models/spree/encashment.rb
@@ -2,14 +2,25 @@ module Spree
   class Encashment < Spree::Base
     include Spree::Commission::Calculator
     include Spree::Commission::PayOut
-    
+
+    AUTHORIZE_ACTION = 'authorize'.freeze
+    PAY_ACTION = 'pay'.freeze
+    HOLD_ACTION = 'hold'.freeze
+    CANCEL_ACTION = 'cancel'.freeze
+
     belongs_to :user
     # belongs_to :remittance
     
+    attr_accessor :action, :action_amount
+
     extend DisplayMoney
-    money_methods :amount
+    money_methods :amount, :amount_used
 
     state_machine :state, initial: :draft do
+      before_transition :on => :authorize, :do => :authorize_action_items
+      before_transition :on => :pay, :do => :pay_action_items
+
+
       event :authorize do
         transition [:draft, :held] => :authorized
       end
@@ -26,5 +37,12 @@ module Spree
         transition all - [:paid, :canceled] => :canceled
       end
     end
+
+    def authorize_action_items
+    end
+
+    def pay_action_items
+    end
+
   end
 end
